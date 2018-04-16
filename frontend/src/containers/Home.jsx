3d9d8f5c17cb443d6b6a543e39878a86d5884334
Allan <allancarlosdeveloper@gmail.com>
2018-04-15 23:39:21 -0300

    feat: create a post button UI

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid } from "semantic-ui-react";
+import { Grid, Icon, Button } from "semantic-ui-react";
 
 import Header from "./../components/Header/Header";
 import PostsList from "./../components/Posts/List";
@@ -7,8 +7,7 @@ import CategoriesList from "./../components/Categories/List";
 
 export default class HomeContainer extends Component {
   render() {
-    return (
-      <Grid columns={1} container>
+    return <Grid columns={1} container>
         <Grid.Column>
           <Header />
         </Grid.Column>
@@ -17,10 +16,16 @@ export default class HomeContainer extends Component {
           <CategoriesList />
         </Grid.Column>
 
+        <Grid.Column mobile={16} computer={3}>
+          <Button fluid color="pink" floated="right" icon>
+            <Icon name="add" />
+            Create a post
+          </Button>
+        </Grid.Column>
+
         <Grid.Column>
           <PostsList />
         </Grid.Column>
-      </Grid>
-    );
+      </Grid>;
   }
 }
