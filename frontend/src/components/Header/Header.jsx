1449cdfe648be4342667836345b080a3d2442b1d
Allan <allancarlosdeveloper@gmail.com>
2018-06-27 19:15:40 -0300

    fix: header title

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Header } from "semantic-ui-react";
 
 const HeaderMenu = () => (
   <Header as={Link} to="/" className="app-header" textAlign="center">
-    <h1>Readble</h1>
+    <h1>Readable</h1>
   </Header>
 );
 
