d497091f780b4aa42f82143f7ce3a2b46eaa180d
Allan <allancarlosdeveloper@gmail.com>
2018-06-28 00:52:52 -0300

    refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ export default class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <RootScreen />} />
+        <Route exact path="/" component={RootScreen} />
         <Route exact path="/new" render={() => <FormPostScreen />} />
         <Route exact path="/:category" render={props => <PostsListScreen {...props} />} />
         <Route exact path="/:category/:id" render={({ match }) => <ShowPostScreen id={match.params.id} />} />
